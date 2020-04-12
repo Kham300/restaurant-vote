843b567dfceb9f2f71912be8ec7b5edda7c82145
Khamidillo <boogaga3000@gmail.com>
2020-04-12 20:27:21 +0300

    Initial commit

---
+++ src/test/java/ru/voter/restaurantvote/RestaurantVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.voter.restaurantvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
