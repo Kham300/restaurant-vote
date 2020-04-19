513cbedd08c10c5954ee05cf12defc1c58bc59e2
Khamidillo <boogaga3000@gmail.com>
2020-04-19 19:27:32 +0300

    data rest and hal

--- src/main/java/ru/voter/restaurantvote/repository/UserRepository.java
+++ src/main/java/ru/voter/restaurantvote/repository/UserRepository.java
@@ -2,6 +2,7 @@ package ru.voter.restaurantvote.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.voter.restaurantvote.model.User;
 
 import java.util.List;
@@ -9,8 +10,10 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    @RestResource(rel = "by-email", path = "by-email")
     @Query("SELECT u FROM User u WHERE u.email = LOWER(:email)")
     Optional<User> findByEmailIgnoreCase(String email);
 
+    @RestResource(rel = "by-lastName", path = "by-lastName")
     List<User> findByLastNameContainingIgnoreCase(String lastName);
 }
